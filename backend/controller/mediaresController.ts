d2ff178497a7398b81c53ebcf09dbb3a571cd8b3
Emanueleiva05 <emanueleiva12@gmail.com>
2026-03-04 20:23:37 -0300

    Mejora en los nombres de metodos y rutas de entregaDetalle

--- backend/controller/mediaresController.ts
+++ backend/controller/mediaresController.ts
@@ -1,101 +1,94 @@
 import { Request, Response, NextFunction } from "express";
-import {
-  deleteMediares,
-  getCarnesMediares,
-  getMediares,
-  getMediaresById,
-  setMediares,
-  updateMediares,
-} from "../services/mediaresService";
+import * as mediaresSevices from "../services/mediaresService";
 
 export const createMediares = async (
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   try {
     const data = req.body;
 
-    const mediares = await setMediares(data);
+    const mediares = await mediaresSevices.createMediares(data);
 
-    res.status(202).json(mediares);
+    res.status(201).json(mediares);
   } catch (err) {
     next(err);
   }
 };
 
-export const modifyMediares = async (
+export const updateMediares = async (
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   try {
     const data = req.body;
     const id = res.locals.id;
 
-    const mediares = await updateMediares(id, data);
+    const mediares = await mediaresSevices.updateMediares(id, data);
 
-    res.status(202).json(mediares);
+    res.status(200).json(mediares);
   } catch (err) {
     next(err);
   }
 };
 
-export const removeMediares = async (
+export const deleteMediares = async (
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   try {
     const id = res.locals.id;
 
-    await deleteMediares(id);
+    await mediaresSevices.deleteMediares(id);
 
-    res.status(202).json({ message: "Mediares eliminada con exito" });
+    res.status(204).json({ message: "Mediares eliminada con exito" });
   } catch (err) {
     next(err);
   }
 };
 
-export const obtainMediaresById = async (
+export const getMediaresById = async (
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   try {
     const id = res.locals.id;
 
-    const mediares = await getMediaresById(id);
+    const mediares = await mediaresSevices.getMediaresById(id);
 
-    res.status(202).json(mediares);
+    res.status(200).json(mediares);
   } catch (err) {
     next(err);
   }
 };
 
-export const obtainMediares = async (
+export const getMediares = async (
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   try {
-    const mediares = await getMediares();
+    const mediares = await mediaresSevices.getMediares();
 
-    res.status(202).json(mediares);
+    res.status(200).json(mediares);
   } catch (err) {
     next(err);
   }
 };
 
-export const obtainCarnesMediares = async (
+export const getCarneByMediares = async (
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   try {
     const id = res.locals.id;
 
-    const carnes = await getCarnesMediares(id);
+    const carnes = await mediaresSevices.getCarneByMediares(id);
 
     res.status(202).json(carnes);
   } catch (err) {
